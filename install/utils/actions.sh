80459a95657c9b09ee8b9759d5512fa2aa964655
Mark Lucernas <marklcrns@gmail.com>
2022-01-29 14:23:46 -0800

    feat(install): Improve tally report
    - pac-logger now support multiple invoke report stacking
      - Multiple invocation count is prepended on each report
      - Add tally skip flag option
    - Improve code reusability with aliases

--- install/utils/actions.sh
+++ install/utils/actions.sh
@@ -38,6 +38,52 @@ include "${EZ_INSTALL_HOME}/common/string.sh"
 include "${EZ_INSTALL_HOME}/install/const.sh"
 
 
+function info()    { _action -l 'info' "${@}"; }
+function ok()      { _action -l 'notice' "${@}"; }
+function skip()    { _action -l 'notice' "${@}"; }
+function warning() { _action -l 'warn' "${@}"; }
+function error()   { _action -l 'warn' "${@}"; }
+function finish()  { _action -l 'warn' -e 0 ${@}; }
+function abort()   { _action -l 'warn' -e 0 ${@}; }
+function crit()    { _action -l 'crit' -e ${BASH_EX_GENERAL} "${@}"; }
+
+function _action() {
+  local depth=1
+  local log="info"
+  local exit_code=
+
+  OPTIND=1
+  while getopts "d:e:l:" opt; do
+    case ${opt} in
+      d) depth=${OPTARG} ;;
+      e) exit_code=${OPTARG} ;;
+      l) log="${OPTARG}" ;;
+      *)
+        error "Invalid flag option(s)"
+        exit $BASH_SYS_EX_USAGE
+    esac
+  done
+  shift "$((OPTIND-1))"
+
+  if [[ -z "${1+x}" ]]; then
+    error "${BASH_SYS_MSG_USAGE_MISSARG}"
+    return $BASH_SYS_EX_USAGE
+  fi
+
+  local message="${1}"
+  [[ -n "${2+x}" ]] && exit_code=${2}
+  depth=$(($depth+1))   # offset wrapper functions
+
+  if ${VERBOSE}; then
+    log "${log}" "$(basename -- "${BASH_SOURCE[${depth}]}").${FUNCNAME[${depth}]}():${BASH_LINENO[${depth}-1]} ${message}"
+  else
+    log "${log}" "${message}"
+  fi
+
+  is_integer $exit_code && exit $exit_code
+}
+
+
 # Execute message then log DEBUG on VERBOSE.
 function execlog() {
   local depth=1
@@ -80,51 +126,4 @@ function execlog() {
 
   return $res
 }
-
-
-function _action() {
-  local depth=1
-  local log="info"
-  local exit_code=
-
-  OPTIND=1
-  while getopts "d:e:l:" opt; do
-    case ${opt} in
-      d) depth=${OPTARG} ;;
-      e) exit_code=${OPTARG} ;;
-      l) log="${OPTARG}" ;;
-      *)
-        error "Invalid flag option(s)"
-        exit $BASH_SYS_EX_USAGE
-    esac
-  done
-  shift "$((OPTIND-1))"
-
-  if [[ -z "${1+x}" ]]; then
-    error "${BASH_SYS_MSG_USAGE_MISSARG}"
-    return $BASH_SYS_EX_USAGE
-  fi
-
-  local message="${1}"
-  [[ -n "${2+x}" ]] && exit_code=${2}
-  depth=$(($depth+1))   # offset wrapper functions
-
-  if ${VERBOSE}; then
-    log "${log}" "$(basename -- "${BASH_SOURCE[${depth}]}").${FUNCNAME[${depth}]}():${BASH_LINENO[${depth}-1]} ${message}"
-  else
-    log "${log}" "${message}"
-  fi
-
-  is_integer $exit_code && exit $exit_code
-}
-
-
-function info()    { _action -l 'info' "${@}"; }
-function ok()      { _action -l 'notice' "${@}"; }
-function skip()    { _action -l 'notice' "${@}"; }
-function warning() { _action -l 'warn' "${@}"; }
-function error()   { _action -l 'warn' "${@}"; }
-function finish()  { _action -l 'warn' -e 0 ${@}; }
-function abort()   { _action -l 'warn' -e 0 ${@}; }
-function crit()    { _action -l 'crit' -e ${BASH_EX_GENERAL} "${@}"; }
 
--- install/utils/pac-logger.sh
+++ install/utils/pac-logger.sh
@@ -25,77 +25,28 @@ include "${EZ_INSTALL_HOME}/install/utils/actions.sh"
 [[ -z "${FAILED_PACKAGES+x}" ]]     && declare -a FAILED_PACKAGES=()
 
 
-function pac_log_success() {
-  if [[ -z "${1+x}" ]]; then
-    error "${BASH_SYS_MSG_USAGE_MISSARG}"
-    return $BASH_SYS_EX_USAGE
-  fi
-  if [[ -z "${2+x}" ]]; then
-    error "${BASH_SYS_MSG_USAGE_MISSARG}"
-    return $BASH_SYS_EX_USAGE
-  fi
+function pac_log_success() { _pac_log -r 'SUCCESSFUL_PACKAGES' -m 'ok' "${@}"; }
+function pac_log_skip()    { _pac_log -r 'SKIPPED_PACKAGES' -m 'skip' "${@}"; }
+function pac_log_failed()  { _pac_log -r 'FAILED_PACKAGES' -m 'error' "${@}"; }
 
-  local manager="${1}"
-  local package="${2}"
-  local message="${3:-}"
+function _pac_log() {
+  local message_command=""
+  local report_array_name=""
+  local skip_tally=false
 
-  if [[ -n "${message}" ]]; then
-    ok -d 2 "${message}"
-  else
-    ok -d 2 "${manager} '${package}' package installation successful"
-  fi
+  OPTIND=1
+  while getopts "m:r:x" opt; do
+    case ${opt} in
+      m) message_command="${OPTARG}" ;;
+      r) report_array_name="${OPTARG}" ;;
+      x) skip_tally=true ;;
+      *)
+        error "Invalid flag option(s)"
+        exit $BASH_SYS_EX_USAGE
+    esac
+  done
+  shift "$((OPTIND-1))"
 
-  local log="'${package}' SUCCESSFUL"
-
-  [[ "${manager}" != 'N/A' ]] && log="${manager} ${log}"
-  if ! has_pac_log_duplicate "${log}" "${SUCCESSFUL_PACKAGES[@]}"; then
-    SUCCESSFUL_PACKAGES=( "${SUCCESSFUL_PACKAGES[@]}" "${log}" )
-  fi
-}
-
-
-function pac_log_skip() {
-  if [[ -z "${1+x}" ]]; then
-    error "${BASH_SYS_MSG_USAGE_MISSARG}"
-    return $BASH_SYS_EX_USAGE
-  fi
-  if [[ -z "${2+x}" ]]; then
-    error "${BASH_SYS_MSG_USAGE_MISSARG}"
-    return $BASH_SYS_EX_USAGE
-  fi
-
-  local exit_code=$1
-  local has_exit_code=false
-  if [[ $exit_code =~ ^[0-9]+$ ]]; then
-    has_exit_code=true
-    shift
-  fi
-
-  local manager="${1}"
-  local package="${2}"
-  local message="${3:-}"
-
-  if [[ -n "${message}" ]]; then
-    skip -d 2 "${message}"
-  else
-    skip -d 2 "${manager} '${package}' package already installed"
-  fi
-
-  local log=""
-  if $has_exit_code; then
-    log="'${package}' SKIPPED ($exit_code)"
-  else
-    log="'${package}' SKIPPED"
-  fi
-
-  [[ "${manager}" != 'N/A' ]] && log="${manager} ${log}"
-  if ! has_pac_log_duplicate "${log}" "${SKIPPED_PACKAGES[@]}"; then
-    SKIPPED_PACKAGES=( "${SKIPPED_PACKAGES[@]}" "${log}" )
-  fi
-}
-
-
-function pac_log_failed() {
   if [[ -z "${1+x}" ]]; then
     error "${BASH_SYS_MSG_USAGE_MISSARG}"
     return $BASH_SYS_EX_USAGE
@@ -107,7 +58,7 @@ function pac_log_failed() {
 
   local exit_code=$1
   local has_exit_code=false
-  if [[ $exit_code =~ ^[0-9]+$ ]]; then
+  if is_integer $exit_code; then
     has_exit_code=true
     shift
   fi
@@ -116,44 +67,76 @@ function pac_log_failed() {
   local package="${2}"
   local message="${3:-}"
 
+  # '-d 3' offsets _pac_log() and its wrapper functions
   if [[ -n "${message}" ]]; then
-    error -d 2 "${message}"
+    ${message_command} -d 3 "${message}"
   else
-    error -d 2 "${manager} '${package}' package installation failed"
+    ${message_command} -d 3 "${manager} '${package}' package installation failed"
   fi
 
   local log=""
   if $has_exit_code; then
-    log="'${package}' FAILED ($exit_code)"
+    log="'${package}' FAILED [$exit_code]"
   else
     log="'${package}' FAILED"
   fi
 
   [[ "${manager}" != 'N/A' ]] && log="${manager} ${log}"
-  if ! has_pac_log_duplicate "${log}" "${FAILED_PACKAGES[@]}"; then
-    FAILED_PACKAGES=( "${FAILED_PACKAGES[@]}" "${log}" )
-  fi
+  ! $skip_tally && handle_duplicates -s "${log}" ${report_array_name}
 }
 
 
-function has_pac_log_duplicate() {
+function handle_duplicates() {
+  local stack=false
+
+  OPTIND=1
+  while getopts "s" opt; do
+    case ${opt} in
+      s) stack=true ;;
+      *)
+        error "Invalid flag option(s)"
+        exit $BASH_SYS_EX_USAGE
+    esac
+  done
+  shift "$((OPTIND-1))"
+
   if [[ -z "${1+x}" ]]; then
     error "${BASH_SYS_MSG_USAGE_MISSARG}"
     return $BASH_SYS_EX_USAGE
   fi
 
+  if [[ -z "${2+x}" ]]; then
+    error "${BASH_SYS_MSG_USAGE_MISSARG}"
+    return $BASH_SYS_EX_USAGE
+  fi
+
   local new_log="${1}"
-  shift 1
-  local log=( "${@}" )
+  local array_name="${2}"
+
+  local log=()
+  eval "log=( \"\${${array_name}[@]}\" )"
 
   local i=
+  local repeat=
+  local stripped=
   for ((i = 0; i < ${#log[@]}; ++i)); do
     if has_substr "${new_log}" "${log[$i]}"; then
+      # Prepend '($repeat)' to new_log
+      if $stack; then
+        repeat="$(echo "${log[$i]}" | sed -e 's/^(\(.*\)).*/\1/')"    # extract repeat
+        stripped="$(echo "${log[$i]}" | sed -e 's/^(\(.*\))\s*//')"   # strip (repeat)
+        if is_integer "${repeat}"; then
+          eval "${array_name}[$i]=\"($(($repeat+1))) ${stripped}\""
+        else
+          eval "${array_name}[$i]=\"(2) ${stripped}\""
+        fi
+      fi
       return $BASH_EX_OK
     fi
   done
 
-  return $BASH_EX_GENERAL
+  # Add new_log to report
+  eval "${array_name}=( \"\${${array_name}[@]}\" \"${new_log}\" )"
 }
 
 
@@ -163,34 +146,63 @@ function pac_report() {
   local skipped_count=0
   local failed_count=0
 
-  local i=
+  local reports=(
+    "SUCCESSFUL_PACKAGES"
+    "SKIPPED_PACKAGES"
+    "FAILED_PACKAGES"
+  )
 
-  if [[ -n ${SUCCESSFUL_PACKAGES[@]} ]]; then
-    echo -e "\n${COLOR_UL_NC}Successful Installations${COLOR_NC}\n"
-    for ((i = 0; i < ${#SUCCESSFUL_PACKAGES[@]}; ++i)); do
-      echo -e "${COLOR_GREEN}${SUCCESSFUL_PACKAGES[$i]}${COLOR_NC}"
-      total_count=$(expr ${total_count} + 1)
-      successful_count=$(expr ${successful_count} + 1)
-    done
-  fi
+  local -A report_desc
+  report_desc['SUCCESSFUL_PACKAGES']="Successful Installations"
+  report_desc['SKIPPED_PACKAGES']="Skipped Installations"
+  report_desc['FAILED_PACKAGES']="Failed Installations"
 
-  if [[ -n ${SKIPPED_PACKAGES[@]} ]]; then
-    echo -e "\n${COLOR_UL_NC}Skipped Installations${COLOR_NC}\n"
-    for ((i = 0; i < ${#SKIPPED_PACKAGES[@]}; ++i)); do
-      echo -e "${COLOR_BLUE}${SKIPPED_PACKAGES[$i]}${COLOR_NC}"
-      total_count=$(expr ${total_count} + 1)
-      skipped_count=$(expr ${skipped_count} + 1)
-    done
-  fi
+  local -A report_colors
+  report_colors['SUCCESSFUL_PACKAGES']="${COLOR_GREEN}"
+  report_colors['SKIPPED_PACKAGES']="${COLOR_BLUE}"
+  report_colors['FAILED_PACKAGES']="${COLOR_RED}"
 
-  if [[ -n ${FAILED_PACKAGES[@]} ]]; then
-    echo -e "\n${COLOR_UL_NC}Failed Installations${COLOR_NC}\n"
-    for ((i = 0; i < ${#FAILED_PACKAGES[@]}; ++i)); do
-      echo -e "${COLOR_RED}${FAILED_PACKAGES[$i]}${COLOR_NC}"
-      total_count=$(expr ${total_count} + 1)
-      failed_count=$(expr ${failed_count} + 1)
-    done
-  fi
+  for report in ${reports[@]}; do
+    local package_reports=()
+    eval "package_reports=( \"\${${report}[@]}\" )"
+
+    if [[ -n "${package_reports[@]}" ]]; then
+      echo -e "\n${COLOR_UL_NC}"${report_desc["${report}"]}"${COLOR_NC}\n"
+      for ((i = 0; i < ${#package_reports[@]}; ++i)); do
+        echo -e "${report_colors["${report}"]}${package_reports[$i]}${COLOR_NC}"
+        total_count=$(expr ${total_count} + 1)
+        successful_count=$(expr ${successful_count} + 1)
+      done
+    fi
+  done
+
+  # DEPRECATED: For reference only
+  # if [[ -n ${SUCCESSFUL_PACKAGES[@]} ]]; then
+  #   echo -e "\n${COLOR_UL_NC}Successful Installations${COLOR_NC}\n"
+  #   for ((i = 0; i < ${#SUCCESSFUL_PACKAGES[@]}; ++i)); do
+  #     echo -e "${COLOR_GREEN}${SUCCESSFUL_PACKAGES[$i]}${COLOR_NC}"
+  #     total_count=$(expr ${total_count} + 1)
+  #     successful_count=$(expr ${successful_count} + 1)
+  #   done
+  # fi
+
+  # if [[ -n ${SKIPPED_PACKAGES[@]} ]]; then
+  #   echo -e "\n${COLOR_UL_NC}Skipped Installations${COLOR_NC}\n"
+  #   for ((i = 0; i < ${#SKIPPED_PACKAGES[@]}; ++i)); do
+  #     echo -e "${COLOR_BLUE}${SKIPPED_PACKAGES[$i]}${COLOR_NC}"
+  #     total_count=$(expr ${total_count} + 1)
+  #     skipped_count=$(expr ${skipped_count} + 1)
+  #   done
+  # fi
+
+  # if [[ -n ${FAILED_PACKAGES[@]} ]]; then
+  #   echo -e "\n${COLOR_UL_NC}Failed Installations${COLOR_NC}\n"
+  #   for ((i = 0; i < ${#FAILED_PACKAGES[@]}; ++i)); do
+  #     echo -e "${COLOR_RED}${FAILED_PACKAGES[$i]}${COLOR_NC}"
+  #     total_count=$(expr ${total_count} + 1)
+  #     failed_count=$(expr ${failed_count} + 1)
+  #   done
+  # fi
 
   cat << EOF
 
@@ -202,4 +214,171 @@ TOTAL      │ ${total_count}
 
 EOF
 }
+
+
+
+# DEPRECATED: For reference only
+# function pac_log_success() {
+#   local skip_tally=false
+
+#   OPTIND=1
+#   while getopts "x" opt; do
+#     case ${opt} in
+#       x) skip_tally=true ;;
+#       *)
+#         error "Invalid flag option(s)"
+#         exit $BASH_SYS_EX_USAGE
+#     esac
+#   done
+#   shift "$((OPTIND-1))"
+
+#   if [[ -z "${1+x}" ]]; then
+#     error "${BASH_SYS_MSG_USAGE_MISSARG}"
+#     return $BASH_SYS_EX_USAGE
+#   fi
+#   if [[ -z "${2+x}" ]]; then
+#     error "${BASH_SYS_MSG_USAGE_MISSARG}"
+#     return $BASH_SYS_EX_USAGE
+#   fi
+
+#   local manager="${1}"
+#   local package="${2}"
+#   local message="${3:-}"
+
+#   if [[ -n "${message}" ]]; then
+#     ok -d 2 "${message}"
+#   else
+#     ok -d 2 "${manager} '${package}' package installation successful"
+#   fi
+
+#   local log="'${package}' SUCCESSFUL"
+
+#   [[ "${manager}" != 'N/A' ]] && log="${manager} ${log}"
+#   ! $skip_tally && handle_duplicates -s "${log}" SUCCESSFUL_PACKAGES
+# }
+
+
+# function pac_log_skip() {
+#   local skip_tally=false
+
+#   OPTIND=1
+#   while getopts "x" opt; do
+#     case ${opt} in
+#       x) skip_tally=true ;;
+#       *)
+#         error "Invalid flag option(s)"
+#         exit $BASH_SYS_EX_USAGE
+#     esac
+#   done
+#   shift "$((OPTIND-1))"
+
+#   if [[ -z "${1+x}" ]]; then
+#     error "${BASH_SYS_MSG_USAGE_MISSARG}"
+#     return $BASH_SYS_EX_USAGE
+#   fi
+#   if [[ -z "${2+x}" ]]; then
+#     error "${BASH_SYS_MSG_USAGE_MISSARG}"
+#     return $BASH_SYS_EX_USAGE
+#   fi
+
+#   local exit_code=$1
+#   local has_exit_code=false
+#   if is_integer $exit_code; then
+#     has_exit_code=true
+#     shift
+#   fi
+
+#   local manager="${1}"
+#   local package="${2}"
+#   local message="${3:-}"
+
+#   if [[ -n "${message}" ]]; then
+#     skip -d 2 "${message}"
+#   else
+#     skip -d 2 "${manager} '${package}' package already installed"
+#   fi
+
+#   local log=""
+#   if $has_exit_code; then
+#     log="'${package}' SKIPPED [$exit_code]"
+#   else
+#     log="'${package}' SKIPPED"
+#   fi
+
+#   [[ "${manager}" != 'N/A' ]] && log="${manager} ${log}"
+#   ! $skip_tally && handle_duplicates -s "${log}" SKIPPED_PACKAGES
+# }
+
+
+# function pac_log_failed() {
+#   local skip_tally=false
+
+#   OPTIND=1
+#   while getopts "x" opt; do
+#     case ${opt} in
+#       x) skip_tally=true ;;
+#       *)
+#         error "Invalid flag option(s)"
+#         exit $BASH_SYS_EX_USAGE
+#     esac
+#   done
+#   shift "$((OPTIND-1))"
+
+#   if [[ -z "${1+x}" ]]; then
+#     error "${BASH_SYS_MSG_USAGE_MISSARG}"
+#     return $BASH_SYS_EX_USAGE
+#   fi
+#   if [[ -z "${2+x}" ]]; then
+#     error "${BASH_SYS_MSG_USAGE_MISSARG}"
+#     return $BASH_SYS_EX_USAGE
+#   fi
+
+#   local exit_code=$1
+#   local has_exit_code=false
+#   if is_integer $exit_code; then
+#     has_exit_code=true
+#     shift
+#   fi
+
+#   local manager="${1}"
+#   local package="${2}"
+#   local message="${3:-}"
+
+#   if [[ -n "${message}" ]]; then
+#     error -d 2 "${message}"
+#   else
+#     error -d 2 "${manager} '${package}' package installation failed"
+#   fi
+
+#   local log=""
+#   if $has_exit_code; then
+#     log="'${package}' FAILED [$exit_code]"
+#   else
+#     log="'${package}' FAILED"
+#   fi
+
+#   [[ "${manager}" != 'N/A' ]] && log="${manager} ${log}"
+#   ! $skip_tally && handle_duplicates -s "${log}" FAILED_PACKAGES
+# }
+
+
+# function has_pac_log_duplicate() {
+#   if [[ -z "${1+x}" ]]; then
+#     error "${BASH_SYS_MSG_USAGE_MISSARG}"
+#     return $BASH_SYS_EX_USAGE
+#   fi
+
+#   local new_log="${1}"
+#   shift 1
+#   local log=( "${@}" )
+
+#   local i=
+#   for ((i = 0; i < ${#log[@]}; ++i)); do
+#     if has_substr "${new_log}" "${log[$i]}"; then
+#       return $BASH_EX_OK
+#     fi
+#   done
+
+#   return $BASH_EX_GENERAL
+# }
 
