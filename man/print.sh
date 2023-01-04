044058c4cfbc780bd64bb321e3afa59842388cda
Mark Lucernas <marklcrns@gmail.com>
2023-01-04 15:34:27 -0800

    docs(all): Modularize usage and manual pages

---
+++ man/print.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ "${0##*/}" == "${BASH_SOURCE[0]##*/}" ]; then
+  echo "WARNING: $(realpath -s $0) is not meant to be executed directly!" >&2;
+  echo "Use this script only by sourcing it." >&2;
+  exit 1
+fi
+
+# Header guard
+[[ -z "${MAN_PRINT_SH_INCLUDED+x}" ]] \
+  && readonly MAN_PRINT_SH_INCLUDED=1 \
+  || return 0
+
+
+function print_page() {
+  eval "cat <<EOF
+$(<${1})
+EOF
+" 2> /dev/null
+}
+
